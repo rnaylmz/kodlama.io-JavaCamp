5ba1029fc7b0450fe56d291038b804c845f778d5
Rana Yılmaz <rnaryhn@gmail.com>
2022-10-19 23:04:07 +0300

    initial commit

---
+++ src/week1/datatypes/Main.java
@@ -0,0 +1,9 @@
+package week1.datatypes;
+
+public class Main {
+
+    public static void main(String[] args){
+        //int, byte,
+        int sayi = 12;
+    }
+}
---
+++ src/week1/variables/Main.java
@@ -0,0 +1,12 @@
+package week1.variables;
+
+public class Main {
+
+    //reusibility: tekrar tekrar kullanılabilirlik.
+
+    public static void main(String[] args) {
+        int ogrenciSayisi = 12;
+        String mesaj = "Öğrenci sayısı : ";
+        System.out.println(mesaj + ogrenciSayisi);
+    }
+}
