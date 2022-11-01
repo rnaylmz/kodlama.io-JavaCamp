72176d5ebb20b259707cff5f085cddf54406b860
Rana Yılmaz <rnaryhn@gmail.com>
2022-11-01 17:22:37 +0300

    initial commit

---
+++ src/week2/inheritanceDemo/AskerKrediManager.java
@@ -0,0 +1,5 @@
+package week2.inheritanceDemo;
+
+public class AskerKrediManager extends BaseKrediManager{
+
+}
--- src/week2/inheritanceDemo/Main.java
+++ src/week2/inheritanceDemo/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         //Değişime direnmeyen programlar yazabilmeliyiz. - Clean Code
 
-
+        KrediUI krediUI = new KrediUI();
+        krediUI.KrediHesapla(new AskerKrediManager());
     }
 }
